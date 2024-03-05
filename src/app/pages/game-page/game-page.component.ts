7e9dc3727db4417eb27351f486d491c3b06f1e67
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-03-06 01:28:35 +0300

    feat: carousel

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,17 +1,42 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, NO_ERRORS_SCHEMA, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TuiCarouselModule} from '@taiga-ui/kit';
 import {GameCardComponent} from '@terralink-demo/pages/game-page/components/game-card/game-card.component';
 import {GamePagerComponent} from '@terralink-demo/pages/game-page/components/game-pager/game-pager.component';
+import {ButtonComponent} from '@terralink-demo/ui';
 
 @Component({
     selector: 'game-page',
     standalone: true,
-    imports: [CommonModule, TuiCarouselModule, GameCardComponent, GamePagerComponent],
+    imports: [CommonModule, TuiCarouselModule, GameCardComponent, GamePagerComponent, ButtonComponent],
+    schemas: [NO_ERRORS_SCHEMA],
     templateUrl: './game-page.component.html',
     styleUrl: './game-page.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GamePageComponent {
-    cardIndex = 0;
+export class GamePageComponent implements AfterViewInit {
+    readonly cardIndex = signal<number>(0);
+
+    ngAfterViewInit(): void {
+        this.handleSwipe();
+    }
+
+    private handleSwipe(): void {
+        const swiperEl = document.querySelector('swiper-container');
+
+        if (!swiperEl) {
+            return;
+        }
+
+        document.documentElement.style.setProperty(
+            '--swiper-container-height',
+            `${swiperEl.getBoundingClientRect().height}px`,
+        );
+
+        swiperEl.addEventListener('swiperslidechange', event => {
+            const [swiper] = (event as any).detail;
+            console.log('s', swiper.activeIndex);
+            this.cardIndex.set(swiper.activeIndex);
+        });
+    }
 }
