0fa1284c722ef6dd8d38b8843fd3dd8b38ac2ea6
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-03-06 16:44:46 +0300

    fix: map size behaviour

--- src/app/pages/map-page/map-page.component.ts
+++ src/app/pages/map-page/map-page.component.ts
@@ -1,11 +1,15 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, inject, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, inject, signal, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SvgIconComponent} from 'angular-svg-icon';
 import {TuiPanModule} from '@taiga-ui/cdk';
 import {ButtonComponent} from '@terralink-demo/ui';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Pages} from '@terralink-demo/models';
-import {MutationObserverModule} from '@ng-web-apis/mutation-observer';
+
+const SVG_CONFIG = {
+    width: 800,
+    height: 535,
+};
 
 @Component({
     selector: 'map-page',
@@ -22,9 +26,15 @@ export class MapPageComponent implements AfterViewInit {
     private readonly route = inject(ActivatedRoute);
     private readonly index = this.route.snapshot.queryParamMap.get('id');
 
+    readonly svgWidth = signal<number>(0);
+    readonly svgHeight = signal<number>(0);
+
+    private get containerEl(): Element {
+        return document.getElementsByClassName('container')[0];
+    }
+
     ngAfterViewInit(): void {
-        this.scrollToCenter();
-        console.log(this.icon);
+        this.initMap();
     }
 
     load(e: any) {
@@ -35,8 +45,16 @@ export class MapPageComponent implements AfterViewInit {
         this.router.navigate([Pages.Game], {queryParams: {id: this.index}});
     }
 
-    private scrollToCenter(): void {
-        const offset = (800 - window.outerWidth) / 2;
-        document.getElementsByClassName('container')[0].scrollTo({left: offset, behavior: 'smooth'});
+    private initMap(): void {
+        const height = this.containerEl.getBoundingClientRect().height;
+        const width = (height / SVG_CONFIG.height) * SVG_CONFIG.width;
+        const offset = (width - window.outerWidth) / 2;
+
+        console.log(height);
+
+        this.svgWidth.set(width);
+        this.svgHeight.set(height);
+
+        setTimeout(() => this.containerEl.scrollTo({left: offset}));
     }
 }
