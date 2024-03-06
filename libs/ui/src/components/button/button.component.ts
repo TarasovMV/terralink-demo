976cbce813a3b56547105444a9ce1adfc76b2b60
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-03-06 22:24:22 +0300

    feat: text + icons

--- libs/ui/src/components/button/button.component.ts
+++ libs/ui/src/components/button/button.component.ts
@@ -1,11 +1,12 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TuiRippleModule} from '@taiga-ui/addon-mobile';
+import {SvgIconComponent} from 'angular-svg-icon';
 
 @Component({
     selector: 'ui-button',
     standalone: true,
-    imports: [CommonModule, TuiRippleModule],
+    imports: [CommonModule, TuiRippleModule, SvgIconComponent],
     templateUrl: './button.component.html',
     styleUrl: './button.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
