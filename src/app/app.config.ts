494c4216fa3bf0767fd99252e56d8e48d15ca794
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-03-14 21:19:38 +0300

    feat: add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {TuiRootModule} from '@taiga-ui/core';
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
 import {PreloadAllModules, provideRouter, withPreloading} from '@angular/router';
 import {appRoutes} from './app.routes';
 import {provideAngularSvgIcon} from 'angular-svg-icon';
 import {HttpClientModule} from '@angular/common/http';
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        importProvidersFrom(HttpClientModule),
         provideAnimations(),
-        provideRouter(appRoutes, withPreloading(PreloadAllModules)),
-        importProvidersFrom(TuiRootModule),
         provideAngularSvgIcon(),
+        importProvidersFrom(TuiRootModule),
+        importProvidersFrom(HttpClientModule),
+        provideRouter(appRoutes, withPreloading(PreloadAllModules)),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
     ],
 };
