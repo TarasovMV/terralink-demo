56feeab8476728aed54185f982ab2e9d70a92f68
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-03-26 00:31:23 +0300

    fix: build

--- src/app/dialogs/scanner/scanner.component.ts
+++ src/app/dialogs/scanner/scanner.component.ts
@@ -1,9 +1,6 @@
 import {AfterViewInit, ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ZXingScannerModule} from '@zxing/ngx-scanner';
-import {BarcodeFormat} from '@zxing/library';
 import {POLYMORPHEUS_CONTEXT} from '@tinkoff/ng-polymorpheus';
-import Result from '@zxing/library/esm/core/Result';
 import {TuiDialogContext} from '@taiga-ui/core';
 import {ButtonComponent} from '@terralink-demo/ui';
 import {Html5Qrcode} from 'html5-qrcode';
@@ -11,14 +8,13 @@ import {Html5Qrcode} from 'html5-qrcode';
 @Component({
     selector: 'scanner',
     standalone: true,
-    imports: [CommonModule, ZXingScannerModule, ButtonComponent],
+    imports: [CommonModule, ButtonComponent],
     templateUrl: './scanner.component.html',
     styleUrl: './scanner.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScannerComponent implements AfterViewInit {
     private readonly context = inject(POLYMORPHEUS_CONTEXT) as TuiDialogContext<string | null>;
-    readonly allowedFormats = [BarcodeFormat.QR_CODE];
 
     ngAfterViewInit(): void {
         const aspectRatio = window.innerWidth / window.innerHeight;
