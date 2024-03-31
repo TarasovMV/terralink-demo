06f8aaf4942a90be72099d32f1dac2638bb36d27
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2024-04-01 00:07:56 +0300

    fix: build

--- libs/models/src/interfaces/user-meta.interface.ts
+++ libs/models/src/interfaces/user-meta.interface.ts
@@ -3,7 +3,7 @@ export interface UserMeta {
     user_id?: string;
     fullname: string;
     email: string;
-    created_at: string;
+    created_at?: string;
     phone_number?: string;
     music_genre?: string | null;
     organization?: string | null;
